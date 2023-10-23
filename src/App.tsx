2de4d815d7b667d08759fad5a20792c6f22a0bb1
alaasassii <alaasassijs@gmail.com>
2023-10-23 18:10:01 +0100

    add new thigns

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium quam dolorem adipisci tenetur fugit, doloribus excepturi asperiores distinctio illo dolorum accusamus omnis at officiis voluptates autem, cum possimus, sed delectus.
     </>
   )
 }
